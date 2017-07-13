83ee6a2d478166f5c544195e1455c71331ed5247
kepler62f <blue.kepler62f@gmail.com>
2017-07-13 22:25:33 +0800

    scripted create, list & show

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,14 +1,55 @@
 // TODO. import TODO Model ;-)
-// const Todo = require('../models/todo')
+const Todo = require('../models/todo')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost:27017/mongo-todo-list'
+
+mongoose.connect(url, {
+  useMongoClient: true
+})
+mongoose.Promise = global.Promise
+
 
 function create (params) {
+
   // create a new TODO and console log the response
+  var newTodo = new Todo({
+    name: params.name,
+    description: params.description,
+    completed: params.completed
+  })
+
+  newTodo.save(function(err, data) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('new param created');
+      console.log('new param', data);
+    }
+  })
+
+  //mongoose.connection.close()
 }
+
 function list () {
   // console log the list of all TODOs
+  Todo.find({},function(err, data) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log(data);
+    }
+  })
 }
 function show (id) {
+
   // find the TODO with this id and console log it
+  Todo.findById(id,function(err, data) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log(data);
+    }
+  })
 }
 function update (id, params) {
   // find the TODO with this id and update it's params. console log the result.
